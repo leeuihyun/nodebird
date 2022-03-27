784f62065296ea984d6be63db1f481bf815f74ec
leeuihyun <meta212121@naver.com>
2022-03-27 23:36:41 +0900

    edit userSaga

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,57 +1,91 @@
 import { takeLatest, put, call, delay, fork, all } from "redux-saga/effects";
+import {
+    LOG_IN_REQUEST,
+    LOG_IN_SUCCESS,
+    LOG_IN_FAILURE,
+    LOG_OUT_REQUEST,
+    LOG_OUT_FAILURE,
+    LOG_OUT_SUCCESS,
+    SIGN_UP_FAILURE,
+    SIGN_UP_SUCCESS,
+    SIGN_UP_REQUEST,
+} from "../reducers/user";
 
 import axios from "axios";
 
-function loginApi(data) {
+function logInApi(data) {
     return axios.post("/login/api");
 }
-function logoutApi(data) {
+function logOutApi(data) {
     return axios.post("/login/api");
 }
+function signUpApi(data) {
+    return axios.post("/signup/api");
+}
+
 //login
-function* login(action) {
+function* logIn(action) {
     try {
-        //const res = yield call(loginApi, action.data);
+        //const res = yield call(logInApi, action.data);
         yield delay(1000);
         yield put({
-            type: "LOG_IN_SUCCESS",
-            payload: action.data,
-        });
-    } catch (err) {
-        console.log(err);
-        yield put({
-            type: "LOG_IN_FAILURE",
-            payload: err.response.data,
-        });
-    }
-}
-
-function* watchLogin() {
-    yield takeLatest("LOG_IN_REQUEST", login);
-}
-//logout
-
-function* logout(action) {
-    try {
-        //const res = yield call(logoutApi, action.data);
-        yield delay(1000);
-        yield put({
-            type: "LOG_OUT_SUCCESS",
+            type: LOG_IN_SUCCESS,
             //payload: res.data,
         });
     } catch (err) {
         console.log(err);
         yield put({
-            type: "LOG_OUT_FAILURE",
-            payload: err.response.data,
+            type: LOG_IN_FAILURE,
+            error: err.response.data,
         });
     }
 }
 
-function* watchLogout() {
-    yield takeLatest("LOG_OUT_REQUEST", logout);
+function* watchLogIn() {
+    yield takeLatest(LOG_IN_REQUEST, logIn);
+}
+//logout
+
+function* logOut(action) {
+    try {
+        //const res = yield call(logoutApi, action.data);
+        yield delay(1000);
+        yield put({
+            type: LOG_OUT_SUCCESS,
+            //payload: res.data,
+        });
+    } catch (err) {
+        console.log(err);
+        yield put({
+            type: LOG_OUT_FAILURE,
+            error: err.response.data,
+        });
+    }
 }
 
+function* watchLogOut() {
+    yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* signUp(action) {
+    try {
+        //const res = yield call(signUpApi, action.payload);
+        yield delay(1000);
+        yield put({
+            type: SIGN_UP_SUCCESS,
+            //payload: res.data,
+        });
+    } catch (err) {
+        console.log(err);
+        yield put({
+            type: SIGN_UP_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
+function* watchSignUp() {
+    yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
 export default function* userSaga() {
-    yield all([fork(watchLogin), fork(watchLogout)]);
+    yield all([fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
 }
