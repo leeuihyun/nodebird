d59ba38146706f39e743328dc6e8edba49d9c199
leeuihyun <meta212121@naver.com>
2022-03-27 23:38:07 +0900

    edit profile page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -3,36 +3,11 @@ import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import NickNameEditForm from "../components/NickNameEditForm";
 import FollowList from "../components/FollowList";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-    const followList = [
-        {
-            nickname: "제로초",
-        },
-        {
-            nickname: "바보",
-        },
-        {
-            nickname: "재우",
-        },
-        {
-            nickname: "양군",
-        },
-    ];
-    const followerList = [
-        {
-            nickname: "제로초1",
-        },
-        {
-            nickname: "바보1",
-        },
-        {
-            nickname: "재우1",
-        },
-        {
-            nickname: "양군1",
-        },
-    ];
+    const { user } = useSelector((state) => state.user);
+
     return (
         <>
             <Head>
@@ -40,8 +15,8 @@ const Profile = () => {
             </Head>
             <AppLayout>
                 <NickNameEditForm />
-                <FollowList header="팔로잉 목록" data={followList} />
-                <FollowList header="팔로우 목록" data={followerList} />
+                <FollowList header="팔로잉" data={user.FollowList} />
+                <FollowList header="팔로우" data={user.Followers} />
             </AppLayout>
         </>
     );
