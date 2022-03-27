19548c28738c1287db0778d3ae313c412e9d791a
leeuihyun <meta212121@naver.com>
2022-03-27 23:38:36 +0900

    edit useInput hook

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -5,5 +5,5 @@ export default (initialValue = null) => {
     const handler = useCallback((e) => {
         setter(e.target.value);
     }, []);
-    return [value, handler];
+    return [value, handler, setter];
 };
