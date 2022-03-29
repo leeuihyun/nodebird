31f41b5716c6b5829addecbde8eb5d422a4ed8da
leeuihyun <meta212121@naver.com>
2022-03-29 17:30:59 +0900

    post saga

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -7,24 +7,37 @@ import {
     ADD_COMMENT_REQUEST,
     ADD_COMMENT_SUCCESS,
     ADD_COMMENT_FAILURE,
+    REMOVE_POST_FAILURE,
+    REMOVE_POST_SUCCESS,
+    REMOVE_POST_REQUEST,
 } from "../reducers/post";
-
+import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from "../reducers/user";
+import shortId from "shortid";
 function addPostApi(data) {
     return axios.post("/api/post", data);
 }
 function addCommentApi(data) {
     return axios.post(`/api/post/${data.postId}`, data);
 }
+function removePostApi(data) {
+    return axios.post(`/api/post/${data.id}`, data);
+}
 
 function* addPost(action) {
     try {
         //const res = yield call(addPostApi, action.payload);
         yield delay(1000);
+        const id = shortId.generate();
         yield put({
             type: ADD_POST_SUCCESS,
             data: {
+                id,
                 content: action.data,
             },
+        });
+        yield put({
+            type: ADD_POST_TO_ME,
+            data: id,
         });
     } catch (err) {
         console.log(err);
@@ -51,13 +64,40 @@ function* addComment(action) {
         });
     }
 }
+
+function* removePost(action) {
+    try {
+        //const res = yield call(removePostApi, action.data);
+        yield delay(1000);
+        yield put({
+            type: REMOVE_POST_SUCCESS,
+            data: action.data,
+        });
+        yield put({
+            type: REMOVE_POST_OF_ME,
+            data: action.data,
+        });
+    } catch (err) {
+        console.log(err);
+        yield put({
+            type: REMOVE_POST_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
 function* watchAddPost() {
     yield takeLatest(ADD_POST_REQUEST, addPost);
 }
 function* watchAddComment() {
     yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
-
+function* watchRemovePost() {
+    yield takeLatest(REMOVE_POST_REQUEST, removePost);
+}
 export default function* postSaga() {
-    yield all([fork(watchAddPost), fork(watchAddComment)]);
+    yield all([
+        fork(watchAddPost),
+        fork(watchAddComment),
+        fork(watchRemovePost),
+    ]);
 }
