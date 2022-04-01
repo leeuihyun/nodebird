5a1e729e7aa144c082f1cfbf2730a7d84dd0ba51
leeuihyun <meta212121@naver.com>
2022-04-02 05:57:09 +0900

    edit back app.js

--- back/app.js
+++ back/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
 const db = require("./models");
+const passportConfig = require("./passport");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 db.sequelize
@@ -10,19 +20,35 @@ db.sequelize
     })
     .catch(console.error);
 
+passportConfig();
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    session({
+        saveUninitialized: false,
+        resave: false,
+        secret: process.env.COOKIE_SECRET,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.get("/", (req, res) => {
     res.send("hello express");
 });
 
-app.get("/posts", (req, res) => {
-    res.json([
-        { id: 1, content: "hello" },
-        { id: 2, content: "hello" },
-        { id: 3, content: "hello" },
-    ]);
+app.get("/", (req, res) => {
+    res.send("hello express");
 });
-
+app.use("/user", userRouter);
 app.use("/post", postRouter);
+
 app.listen(3065, () => {
     console.log("server listening");
 });
