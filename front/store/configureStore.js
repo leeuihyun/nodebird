eada83c4f4d08ebf9997c0764ec0ae4aa3f8554a
leeuihyun <meta212121@naver.com>
2022-04-06 01:34:26 +0900

    + store

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -5,7 +5,8 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "../sagas";
 
-const configureStore = () => {
+const configureStore = (text) => {
+    console.log(text);
     const sagaMiddleware = createSagaMiddleware();
     const middleware = [sagaMiddleware];
     const enhancer =
