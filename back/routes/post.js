f25a0439bbfd8d973af38e2db88181533c61b258
leeuihyun <meta212121@naver.com>
2022-04-06 22:48:19 +0900

    edit post.js (add liker)

--- back/routes/post.js
+++ back/routes/post.js
@@ -26,7 +26,13 @@ router.post("/", isLoggedIn, async (req, res, next) => {
                 },
                 {
                     model: User,
+
                     attributes: ["id", "nickname"],
+                },
+                {
+                    model: User, //좋아요 누른 사람,
+                    as: "Likers",
+                    attributes: ["id"],
                 },
             ],
         });
@@ -70,6 +76,33 @@ router.post("/:postId/comment", isLoggedIn, async (req, res, next) => {
     }
 });
 
+router.patch("/:postId/like", async (req, res, next) => {
+    try {
+        const post = await Post.findOne({ where: { id: req.params.postId } });
+        if (!post) {
+            return res.status(403).send("게시글이 존재하지 않습니다.");
+        }
+        await post.addLikers(req.user.id);
+        res.json({ PostId: post.id, UserId: req.user.id });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete("/:postId/unlike", async (req, res, next) => {
+    try {
+        const post = await Post.findOne({ where: { id: req.params.postId } });
+        if (!post) {
+            return res.status(403).send("게시글이 존재하지 않습니다.");
+        }
+        await post.removeLikers(req.user.id);
+        res.json({ PostId: post.id, UserId: req.user.id });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 router.delete("/", (req, res) => {
     res.json({ id: 1 });
 });
