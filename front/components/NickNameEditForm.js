e080778263c714eacb1b33690d4b5857b9c28bcf
leeuihyun <meta212121@naver.com>
2022-04-07 19:24:46 +0900

    edit NicknameEditForm (change nickname)

--- front/components/NickNameEditForm.js
+++ front/components/NickNameEditForm.js
@@ -1,8 +1,20 @@
-import React, { useMemo, useCallback } from "react";
+import React, { useMemo, useCallback, useState } from "react";
 import { Form, Input } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { CHANGE_NICKNAME_REQUEST } from "../reducers/user";
 
 function NickNameEditForm() {
-    const onSubmitForm = useCallback(() => {}, []);
+    const dispatch = useDispatch();
+    const [nickname, setNickname] = useState("");
+    const onChangeNickname = useCallback((e) => {
+        setNickname(e.target.value);
+    }, []);
+    const onSubmitForm = useCallback(() => {
+        dispatch({
+            type: CHANGE_NICKNAME_REQUEST,
+            data: nickname,
+        });
+    }, []);
     const style = useMemo(
         () => ({
             marginBottom: "20px",
@@ -12,8 +24,14 @@ function NickNameEditForm() {
         []
     );
     return (
-        <Form onFinish={onSubmitForm} style={style}>
-            <Input.Search addonBefore="닉네임" enterButton="수정" />
+        <Form style={style}>
+            <Input.Search
+                value={nickname}
+                addonBefore="닉네임"
+                enterButton="수정"
+                onChange={onChangeNickname}
+                onSearch={onSubmitForm}
+            />
         </Form>
     );
 }
