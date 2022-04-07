077b58525451cc6a20fe45c148533e8b2143d916
leeuihyun <meta212121@naver.com>
2022-04-08 02:10:19 +0900

    edit profile page (+loadFollowers/Followings)

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,19 +2,32 @@ import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import NickNameEditForm from "../components/NickNameEditForm";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Router from "next/router";
 import FollowList from "../components/FollowList";
+import {
+    LOAD_FOLLOWERS_REQUEST,
+    LOAD_FOLLOWINGS_REQUEST,
+} from "../reducers/user";
 
 const Profile = () => {
     const { user } = useSelector((state) => state.user);
-
+    const dispatch = useDispatch();
     useEffect(() => {
         if (!(user && user.id)) {
             Router.push("/");
         }
     }, [user && user.id]);
+
+    useEffect(() => {
+        dispatch({
+            type: LOAD_FOLLOWERS_REQUEST,
+        });
+        dispatch({
+            type: LOAD_FOLLOWINGS_REQUEST,
+        });
+    }, []);
     if (!user) {
         return null;
     }
