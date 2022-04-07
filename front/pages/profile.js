a56fdffb00e23bdb70597aac121eb9f000acebbe
leeuihyun <meta212121@naver.com>
2022-04-07 19:25:18 +0900

    fix click F5 -> maintain page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,18 +2,22 @@ import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import NickNameEditForm from "../components/NickNameEditForm";
-import FollowList from "../components/FollowList";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import Router from "next/router";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
     const { user } = useSelector((state) => state.user);
+
     useEffect(() => {
-        if (user && user.id) {
+        if (!(user && user.id)) {
             Router.push("/");
         }
-    });
+    }, [user && user.id]);
+    if (!user) {
+        return null;
+    }
     return (
         <>
             <Head>
@@ -21,7 +25,7 @@ const Profile = () => {
             </Head>
             <AppLayout>
                 <NickNameEditForm />
-                <FollowList header="팔로잉" data={user.FollowList} />
+                <FollowList header="팔로잉" data={user.Followings} />
                 <FollowList header="팔로우" data={user.Followers} />
             </AppLayout>
         </>
