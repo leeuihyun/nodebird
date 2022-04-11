27244c574af5319d1ce7f1ffec1240895786cc29
leeuihyun <meta212121@naver.com>
2022-04-11 22:24:35 +0900

    edit post saga + loadUserPosts, loadHashtagPosts

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -10,6 +10,9 @@ import {
     REMOVE_POST_FAILURE,
     REMOVE_POST_SUCCESS,
     REMOVE_POST_REQUEST,
+    LOAD_POSTS_REQUEST,
+    LOAD_POSTS_SUCCESS,
+    LOAD_POSTS_FAILURE,
     LOAD_POST_REQUEST,
     LOAD_POST_SUCCESS,
     LOAD_POST_FAILURE,
@@ -22,6 +25,12 @@ import {
     UPLOAD_IMAGES_REQUEST,
     UPLOAD_IMAGES_SUCCESS,
     UPLOAD_IMAGES_FAILURE,
+    LOAD_USER_POSTS_REQUEST,
+    LOAD_USER_POSTS_FAILURE,
+    LOAD_USER_POSTS_SUCCESS,
+    LOAD_HASHTAG_POSTS_REQUEST,
+    LOAD_HASHTAG_POSTS_SUCCESS,
+    LOAD_HASHTAG_POSTS_FAILURE,
     RETWEET_REQUEST,
     RETWEET_SUCCESS,
     RETWEET_FAILURE,
@@ -37,8 +46,11 @@ function addCommentApi(data) {
 function removePostApi(data) {
     return axios.delete(`/post/${data}`);
 }
-function loadPostApi(lastId) {
+function loadPostsApi(lastId) {
     return axios.get(`/posts?lastId=${lastId || 0}`);
+}
+function loadPostApi(data) {
+    return axios.get(`/post/${data}`);
 }
 function likePostApi(data) {
     return axios.patch(`/post/${data}/like`);
@@ -51,6 +63,14 @@ function uploadImagesApi(data) {
 }
 function retweetApi(data) {
     return axios.post(`/post/${data}/retweet`);
+}
+function loadUserPostsAPI(data, lastId) {
+    return axios.get(`/user/${data}/posts?lastId=${lastId || 0}`);
+}
+function loadHashtagPostsAPI(data, lastId) {
+    return axios.get(
+        `/hashtag/${encodeURIComponent(data)}?lastId=${lastId || 0}`
+    );
 }
 function* addPost(action) {
     try {
@@ -113,17 +133,17 @@ function* removePost(action) {
     }
 }
 
-function* loadPost(action) {
+function* loadPosts(action) {
     try {
-        const res = yield call(loadPostApi, action.lastId);
+        const res = yield call(loadPostsApi, action.lastId);
         yield put({
-            type: LOAD_POST_SUCCESS,
+            type: LOAD_POSTS_SUCCESS,
             data: res.data,
         });
     } catch (err) {
         console.log(err);
         yield put({
-            type: LOAD_POST_FAILURE,
+            type: LOAD_POSTS_FAILURE,
             error: err.response.data,
         });
     }
@@ -194,6 +214,60 @@ function* retweet(action) {
         });
     }
 }
+function* loadPost(action) {
+    try {
+        const res = yield call(loadPostApi, action.data);
+        console.log("액션데이터");
+        console.log(action.data);
+        console.log("서버데이터");
+        console.log(res.data);
+        yield put({
+            type: LOAD_POST_SUCCESS,
+            data: res.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: LOAD_POST_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+function* loadUserPosts(action) {
+    try {
+        const result = yield call(loadUserPostsAPI, action.data, action.lastId);
+        yield put({
+            type: LOAD_USER_POSTS_SUCCESS,
+            data: result.data,
+        });
+    } catch (err) {
+        console.error(err);
+        yield put({
+            type: LOAD_USER_POSTS_FAILURE,
+            data: err.response.data,
+        });
+    }
+}
+function* loadHashtagPosts(action) {
+    try {
+        const result = yield call(
+            loadHashtagPostsAPI,
+            action.data,
+            action.lastId
+        );
+        yield put({
+            type: LOAD_HASHTAG_POSTS_SUCCESS,
+            data: result.data,
+        });
+    } catch (err) {
+        console.error(err);
+        yield put({
+            type: LOAD_HASHTAG_POSTS_FAILURE,
+            data: err.response.data,
+        });
+    }
+}
+
 function* watchUnlikePost() {
     yield takeLatest(UNLIKE_POST_REQUEST, unlikePost);
 }
@@ -206,14 +280,24 @@ function* watchAddComment() {
 function* watchRemovePost() {
     yield takeLatest(REMOVE_POST_REQUEST, removePost);
 }
-function* watchLoadPost() {
-    yield takeLatest(LOAD_POST_REQUEST, loadPost);
+function* watchLoadPosts() {
+    yield takeLatest(LOAD_POSTS_REQUEST, loadPosts);
 }
 function* watchUploadImages() {
     yield takeLatest(UPLOAD_IMAGES_REQUEST, uploadImages);
 }
 function* watchRetweet() {
     yield takeLatest(RETWEET_REQUEST, retweet);
+}
+function* watchLoadPost() {
+    yield takeLatest(LOAD_POST_REQUEST, loadPost);
+}
+
+function* watchLoadUserPosts() {
+    yield takeLatest(LOAD_USER_POSTS_REQUEST, loadUserPosts);
+}
+function* watchLoadHashtagPosts() {
+    yield takeLatest(LOAD_HASHTAG_POSTS_REQUEST, loadHashtagPosts);
 }
 export default function* postSaga() {
     yield all([
@@ -222,8 +306,11 @@ export default function* postSaga() {
         fork(watchAddPost),
         fork(watchAddComment),
         fork(watchRemovePost),
-        fork(watchLoadPost),
+        fork(watchLoadPosts),
         fork(watchUploadImages),
         fork(watchRetweet),
+        fork(watchLoadPost),
+        fork(watchLoadUserPosts),
+        fork(watchLoadHashtagPosts),
     ]);
 }
