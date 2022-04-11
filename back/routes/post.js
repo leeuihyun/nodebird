558f1f3190f353aeac71fc2dc74c3f22514e9977
leeuihyun <meta212121@naver.com>
2022-04-11 22:20:22 +0900

    post.js server post route

--- back/routes/post.js
+++ back/routes/post.js
@@ -129,6 +129,82 @@ router.post("/:postId/comment", isLoggedIn, async (req, res, next) => {
         next(error);
     }
 });
+
+router.get("/:postId/", async (req, res, next) => {
+    // GET / post/1
+
+    try {
+        const post = await Post.findOne({
+            where: { id: req.params.postId },
+        });
+
+        if (!post) {
+            return res.status(404).send("존재하지 않는 게시글입니다.");
+        }
+
+        const fullPost = await Post.findOne({
+            where: { id: post.id },
+
+            include: [
+                {
+                    model: Post,
+
+                    as: "Retweet",
+
+                    include: [
+                        {
+                            model: User,
+
+                            attributes: ["id", "nickname"],
+                        },
+                        {
+                            model: Image,
+                        },
+                    ],
+                },
+                {
+                    model: User,
+
+                    attributes: ["id", "nickname"],
+                },
+                {
+                    model: User,
+
+                    as: "Likers",
+
+                    attributes: ["id", "nickname"],
+                },
+                {
+                    model: Image,
+                },
+                {
+                    model: Comment,
+
+                    include: [
+                        {
+                            model: User,
+
+                            attributes: ["id", "nickname"],
+                        },
+                    ],
+                },
+                {
+                    model: User,
+
+                    as: "Likers",
+
+                    attributes: ["id"],
+                },
+            ],
+        });
+
+        res.status(200).json(fullPost);
+    } catch (error) {
+        console.error(error);
+
+        next(error);
+    }
+});
 router.post("/:postId/retweet", isLoggedIn, async (req, res, next) => {
     // POST /post/1/retweet
     try {
