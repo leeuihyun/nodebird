dd1f57bcb920f6d3b3cb4faea6345ef1a5c2d512
leeuihyun <meta212121@naver.com>
2022-04-11 22:25:03 +0900

    index page

--- front/pages/index.js
+++ front/pages/index.js
@@ -4,29 +4,22 @@ import AppLayout from "../components/AppLayout";
 import { useSelector, useDispatch } from "react-redux";
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
+import axios from "axios";
 import { useEffect } from "react";
-import { LOAD_POST_REQUEST } from "../reducers/post";
-import { LOAD_USER_REQUEST } from "../reducers/user";
-
+import { LOAD_POSTS_REQUEST } from "../reducers/post";
+import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import { END } from "redux-saga";
+import wrapper from "../store/configureStore";
 const Home = () => {
     const dispatch = useDispatch();
     const { user } = useSelector((state) => state.user);
-    const { mainPosts, hasMorePost, loadPostLoading, retweetError } =
+    const { mainPosts, hasMorePost, loadPostsLoading, retweetError } =
         useSelector((state) => state.post);
     useEffect(() => {
         if (retweetError) {
             alert(retweetError);
         }
     }, [retweetError]);
-    useEffect(() => {
-        dispatch({
-            type: LOAD_USER_REQUEST,
-        });
-        dispatch({
-            type: LOAD_POST_REQUEST,
-            //data :
-        });
-    }, []);
     useEffect(() => {
         function onScroll() {
             /* console.log(
@@ -38,10 +31,10 @@ const Home = () => {
                 window.scrollY + document.documentElement.clientHeight >
                 document.documentElement.scrollHeight - 300
             )
-                if (hasMorePost && !loadPostLoading) {
+                if (hasMorePost && !loadPostsLoading) {
                     const lastId = mainPosts[mainPosts.length - 1]?.id; //없으면 undefined
                     dispatch({
-                        type: LOAD_POST_REQUEST,
+                        type: LOAD_POSTS_REQUEST,
                         lastId,
                     });
                 }
@@ -50,7 +43,7 @@ const Home = () => {
         return () => {
             window.removeEventListener("scroll", onScroll);
         };
-    }, [hasMorePost, loadPostLoading, mainPosts]);
+    }, [hasMorePost, loadPostsLoading, mainPosts]);
     return (
         <AppLayout>
             <Head>
@@ -63,5 +56,24 @@ const Home = () => {
         </AppLayout>
     );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+    async (context) => {
+        const cookie = context.req ? context.req.headers.cookie : "";
+        axios.defaults.headers.Cookie = "";
+        if (context.req && cookie) {
+            axios.defaults.headers.Cookie = cookie;
+        }
+        console.log(context);
+        context.store.dispatch({
+            type: LOAD_MY_INFO_REQUEST,
+        });
+        context.store.dispatch({
+            type: LOAD_POSTS_REQUEST,
+        });
+        context.store.dispatch(END);
+        await context.store.sagaTask.toPromise();
+    }
+); //홈 보다 먼저 실행되는 서버사이드렌더링
 
 export default Home;
