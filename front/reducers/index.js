d280586cf6b75406c98ce2afd4493cd01f0ca6e5
leeuihyun <meta212121@naver.com>
2022-04-11 22:38:14 +0900

    edit index reducer

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -3,18 +3,19 @@ import { combineReducers } from "redux";
 import user from "./user";
 import post from "./post";
 
-const rootReducer = combineReducers({
-    index: (state = {}, action) => {
-        switch (action.type) {
-            case HYDRATE:
-                console.log("HYDRATE", action);
-                return { ...state, ...action.payload };
-            default:
-                return state;
+const rootReducer = (state, action) => {
+    switch (action.type) {
+        case HYDRATE:
+            console.log("HYDRATE", action);
+            return action.payload;
+        default: {
+            const combinedReducer = combineReducers({
+                user,
+                post,
+            });
+            return combinedReducer(state, action);
         }
-    },
-    user,
-    post,
-});
+    }
+};
 
 export default rootReducer;
