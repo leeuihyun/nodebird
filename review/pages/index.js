816693bd08cfef7f0a8bfd9fdf1e7928fb8eaee2
leeuihyun <meta212121@naver.com>
2022-04-13 19:03:49 +0900

    update index.js

--- review/pages/index.js
+++ review/pages/index.js
@@ -1,7 +1,20 @@
 import AppLayout from "../components/Applayout";
+import { useSelector } from "react-redux";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
 const Home = () => {
-    return <AppLayout>HOME</AppLayout>;
+    const { user } = useSelector((state) => state.user);
+    const { mainPosts } = useSelector((state) => state.post);
+
+    return (
+        <AppLayout>
+            {user && <PostForm />}
+            {mainPosts.map((post) => (
+                <PostCard key={post.id} post={post}></PostCard>
+            ))}
+        </AppLayout>
+    );
 };
 
 export default Home;
