fa9af0fadf4df33ff3e71b37cc3c26a0c025671d
leeuihyun <meta212121@naver.com>
2022-04-13 19:04:21 +0900

    edit profile page

--- review/pages/profile.js
+++ review/pages/profile.js
@@ -1,6 +1,23 @@
 import AppLayout from "../components/Applayout";
-
+import FollowList from "../components/FollowList";
+import NicknameEditForm from "../components/NicknameEditForm";
 const Profile = () => {
-    return <AppLayout>Profile</AppLayout>;
+    const followerList = [
+        { nickname: "lee" },
+        { nickname: "vanc" },
+        { nickname: "su" },
+    ];
+    const followingList = [
+        { nickname: "han" },
+        { nickname: "pang" },
+        { nickname: "mr" },
+    ];
+    return (
+        <AppLayout>
+            <NicknameEditForm />
+            <FollowList header="팔로우" data={followerList} />
+            <FollowList header="팔로잉" data={followingList} />
+        </AppLayout>
+    );
 };
 export default Profile;
