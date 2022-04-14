2aba83a58674864ba72c481e2168f8b8198d1b5f
leeuihyun <meta212121@naver.com>
2022-04-14 20:07:50 +0900

    edit post.js dummyPost,dummyComment

--- review/reducers/post.js
+++ review/reducers/post.js
@@ -1,5 +1,6 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import shortId from "shortid";
 
 const initialState = {
     mainPosts: [
@@ -12,17 +13,13 @@ const initialState = {
             content: "first post",
             Images: [
                 {
-                    Images: [
-                        {
-                            src: "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726",
-                        },
-                        {
-                            src: "https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg",
-                        },
-                        {
-                            src: "https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg",
-                        },
-                    ],
+                    src: "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726",
+                },
+                {
+                    src: "https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg",
+                },
+                {
+                    src: "https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg",
                 },
             ],
             Comments: [
@@ -52,30 +49,56 @@ const initialState = {
     addPostLoading: false,
     addPostDone: false,
     addPostError: null,
+    addCommentLoading: false,
+    addCommentDone: false,
+    addCommentError: null,
 };
-const dummyPost = {
-    id: 2,
+
+const dummyPost = (data) => ({
+    id: shortId.generate(),
     User: {
-        id: 2,
-        nickname: "vanc",
+        id: data.User.id,
+        nickname: data.User.nickname,
     },
-    content: "더미데이터입니다.",
+    content: data.content,
     Images: [],
     Comments: [],
-};
+});
+
+const dummyComment = (data) => ({
+    User: {
+        id: data.User.id,
+        nickname: data.User.nickname,
+    },
+    content: data.content,
+});
 export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
 export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
 export const ADD_POST_FAILURE = "ADD_POST_FAILURE";
+
+export const ADD_COMMENT_REQUEST = "ADD_COMMENT_REQUEST";
+export const ADD_COMMENT_SUCCESS = "ADD_COMMENT_SUCCESS";
+export const ADD_COMMENT_FAILURE = "ADD_COMMENT_FAILURE";
 
 const post = handleActions(
     {
         [ADD_POST_REQUEST]: (state, action) =>
             produce(state, (draft) => {
-                draft.mainPosts.unshift(dummyPost);
+                draft.mainPosts.unshift(dummyPost(action.data));
                 draft.postAdded = true;
                 draft.addPostLoading = true;
                 draft.addPostDone = false;
                 draft.addPostError = null;
+            }),
+        [ADD_COMMENT_REQUEST]: (state, action) =>
+            produce(state, (draft) => {
+                const post = draft.mainPosts.find(
+                    (v) => v.id === action.data.id
+                );
+                post.Comments.unshift(dummyComment(action.data));
+                draft.addCommentLoading = true;
+                draft.addCommentDone = false;
+                draft.addCommentError = null;
             }),
     },
     initialState
