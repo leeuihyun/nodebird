ef4f1054687445ae5d6ba0b7e2426126984d66c6
leeuihyun <meta212121@naver.com>
2022-04-15 20:17:37 +0900

    edit ImagesZoom.js

--- review/components/PostImages.js
+++ review/components/PostImages.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { PlusOutlined } from "@ant-design/icons";
+import ImagesZoom from "./imagesZoom";
 
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -8,7 +9,9 @@ const PostImages = ({ images }) => {
     const onZoom = useCallback(() => {
         setShowImagesZoom(true);
     }, []);
-
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, []);
     if (images.length === 1) {
         return (
             <>
@@ -18,6 +21,9 @@ const PostImages = ({ images }) => {
                     alt={images[0].src}
                     onClick={onZoom}
                 />
+                {showImagesZoom && (
+                    <ImagesZoom images={images} onClose={onClose}></ImagesZoom>
+                )}
             </>
         );
     }
@@ -38,6 +44,7 @@ const PostImages = ({ images }) => {
                     width="50%"
                     onClick={onZoom}
                 />
+                <ImagesZoom images={images} onClose={onClose}></ImagesZoom>
             </div>
         );
     }
@@ -66,6 +73,7 @@ const PostImages = ({ images }) => {
                     {images.length - 1}개의 사진 더보기
                 </div>
             </div>
+            <ImagesZoom images={images} onClose={onClose}></ImagesZoom>
         </>
     );
 };
