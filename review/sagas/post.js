01b879c40a9c3eed6d45b5b1547d713a82c37ee5
leeuihyun <meta212121@naver.com>
2022-04-23 00:30:00 +0900

    post sagas

--- review/sagas/post.js
+++ review/sagas/post.js
@@ -1,6 +1,85 @@
-import { delay, all, fork, put } from "redux-saga/effects";
-function* watchAddPost() {}
-function* watchRemovePost() {}
+import axios from "axios";
+import { delay, all, fork, put, takeLatest } from "redux-saga/effects";
+import {
+    ADD_COMMENT_FAILURE,
+    ADD_COMMENT_REQUEST,
+    ADD_COMMENT_SUCCESS,
+    ADD_POST_FAILURE,
+    ADD_POST_REQUEST,
+    ADD_POST_SUCCESS,
+    REMOVE_POST_FAILURE,
+    REMOVE_POST_REQUEST,
+    REMOVE_POST_SUCCESS,
+} from "../reducers/post";
+
+function addPostApi() {
+    axios.post("/api");
+}
+function* addPost(action) {
+    try {
+        yield delay(1000);
+        yield put({
+            type: ADD_POST_SUCCESS,
+            data: action.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: ADD_POST_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+function* watchAddPost() {
+    yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+function addCommentApi() {
+    axios.post("/api");
+}
+
+function* addComment(action) {
+    try {
+        yield delay(1000);
+        yield put({
+            type: ADD_COMMENT_SUCCESS,
+            data: action.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: ADD_COMMENT_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+function removePostApi() {
+    axios.post("/api");
+}
+function* removePost(action) {
+    try {
+        yield delay(1000);
+        yield put({
+            type: REMOVE_POST_SUCCESS,
+            data: action.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: REMOVE_POST_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+function* watchRemovePost() {
+    yield takeLatest(REMOVE_POST_REQUEST, removePost);
+}
+function* watchAddComment() {
+    yield takeLatest(ADD_COMMENT_REQUEST, addComment);
+}
 export default function* postSaga() {
-    yield all([fork(watchAddPost), fork(watchRemovePost)]);
+    yield all([
+        fork(watchAddPost),
+        fork(watchRemovePost),
+        fork(watchAddComment),
+    ]);
 }
