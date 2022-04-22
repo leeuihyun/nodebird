f90184744fc4976b95a42d3c2034c786dec224f7
leeuihyun <meta212121@naver.com>
2022-04-22 22:20:39 +0900

    userSaga redux

--- review/sagas/user.js
+++ review/sagas/user.js
@@ -1,1 +1,62 @@
-export default function* userSaga() {}
+import axios from "axios";
+import { delay, call, all, fork, put, takeLatest } from "redux-saga/effects";
+import {
+    LOG_IN_SUCCESS,
+    LOG_IN_REQUEST,
+    LOG_IN_FAILURE,
+    LOG_OUT_REQUEST,
+    LOG_OUT_SUCCESS,
+    LOG_OUT_FAILURE,
+} from "../reducers/user";
+
+function logInApi() {
+    return axios.post("/api");
+}
+function* logIn(action) {
+    //const res = yield call(logInApi, action.data);
+    try {
+        yield delay(1000);
+        yield put({
+            type: LOG_IN_SUCCESS,
+            data: action.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: LOG_IN_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+
+function* watchLogIn() {
+    yield takeLatest(LOG_IN_REQUEST, logIn);
+}
+
+function logOutApi() {
+    return axios.post("/api");
+}
+function* logOut(action) {
+    //const res = yield call(logOutApi, action.data);
+    try {
+        yield delay(1000);
+        yield put({
+            type: LOG_OUT_SUCCESS,
+            data: action.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: LOG_OUT_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+
+function* watchLogOut() {
+    yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+export default function* userSaga() {
+    yield all([fork(watchLogIn), fork(watchLogOut)]);
+}
