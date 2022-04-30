d4f61a06aa2a8d80d220c51c3406c6ba319f8476
leeuihyun <meta212121@naver.com>
2022-04-30 20:28:43 +0900

    profile page with store

--- review/pages/profile.js
+++ review/pages/profile.js
@@ -1,22 +1,15 @@
 import AppLayout from "../components/Applayout";
 import FollowList from "../components/FollowList";
 import NicknameEditForm from "../components/NicknameEditForm";
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-    const followerList = [
-        { nickname: "lee" },
-        { nickname: "vanc" },
-        { nickname: "su" },
-    ];
-    const followingList = [
-        { nickname: "han" },
-        { nickname: "pang" },
-        { nickname: "mr" },
-    ];
+    const { user } = useSelector((state) => state.user);
     return (
         <AppLayout>
             <NicknameEditForm />
-            <FollowList header="팔로우" data={followerList} />
-            <FollowList header="팔로잉" data={followingList} />
+            <FollowList header="팔로우" data={user.Followers} />
+            <FollowList header="팔로잉" data={user.Followings} />
         </AppLayout>
     );
 };
