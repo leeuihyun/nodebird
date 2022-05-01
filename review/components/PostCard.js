3f7f14f5537b602c6bc26ac4139e789bcffe3e04
leeuihyun <meta212121@naver.com>
2022-05-01 16:53:19 +0900

    edit PostCard components fix

--- review/components/PostCard.js
+++ review/components/PostCard.js
@@ -10,12 +10,13 @@ import {
 } from "@ant-design/icons";
 import styled from "styled-components";
 import Link from "next/link";
-import { useSelector } from "react-redux";
-
 import CommentForm from "./CommentForm";
 import PostCardContent from "./PostCardContent";
 import PostImages from "./PostImages";
 import FollowButton from "./FollowButton";
+
+import { useSelector, useDispatch } from "react-redux";
+import { REMOVE_POST_REQUEST } from "../reducers/post";
 
 const CardWrapper = styled.div`
     margin-bottom: 20px;
@@ -24,9 +25,10 @@ const CardWrapper = styled.div`
 const PostCard = ({ post }) => {
     const [commentFormOpened, setCommentFormOpened] = useState(false);
     const id = useSelector((state) => state.user.user?.id);
-
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.user);
     const [liked, setLiked] = useState(false);
-
+    const { removePostLoading } = useSelector((state) => state.post);
     const onToggleLike = useCallback(() => {
         setLiked((prev) => !prev);
     }, []);
@@ -34,7 +36,12 @@ const PostCard = ({ post }) => {
     const onToggleComment = useCallback(() => {
         setCommentFormOpened((prev) => !prev);
     }, []);
-
+    const onClickRemove = useCallback(() => {
+        dispatch({
+            type: REMOVE_POST_REQUEST,
+            data: post.id,
+        });
+    }, []);
     return (
         <CardWrapper key={post.id}>
             <Card
@@ -58,7 +65,13 @@ const PostCard = ({ post }) => {
                                 {id && post.User.id === id ? (
                                     <>
                                         <Button>수정</Button>
-                                        <Button type="danger">삭제</Button>
+                                        <Button
+                                            type="danger"
+                                            onClick={onClickRemove}
+                                            loading={removePostLoading}
+                                        >
+                                            삭제
+                                        </Button>
                                     </>
                                 ) : (
                                     <Button>신고</Button>
@@ -104,7 +117,7 @@ const PostCard = ({ post }) => {
 
 PostCard.propTypes = {
     post: PropTypes.shape({
-        id: PropTypes.number,
+        id: PropTypes.string,
         User: PropTypes.object,
         content: PropTypes.string,
         createdAt: PropTypes.object,
