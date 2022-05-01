98fbf81d4ef5f5d797d46aa00d93738a74cde356
leeuihyun <meta212121@naver.com>
2022-05-01 20:31:11 +0900

    edit post infinite scrolling

--- review/sagas/post.js
+++ review/sagas/post.js
@@ -7,6 +7,10 @@ import {
     ADD_POST_FAILURE,
     ADD_POST_REQUEST,
     ADD_POST_SUCCESS,
+    generateDummyData,
+    LOAD_POST_FAILURE,
+    LOAD_POST_REQUEST,
+    LOAD_POST_SUCCESS,
     REMOVE_POST_FAILURE,
     REMOVE_POST_REQUEST,
     REMOVE_POST_SUCCESS,
@@ -90,10 +94,33 @@ function* watchRemovePost() {
 function* watchAddComment() {
     yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
+
+function loadPostApi() {
+    axios.post("/api");
+}
+function* loadPost(action) {
+    try {
+        yield delay(1000);
+        yield put({
+            type: LOAD_POST_SUCCESS,
+            data: generateDummyData(10),
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: LOAD_POST_FAILURE,
+            error: error.response.data,
+        });
+    }
+}
+function* watchLoadPost() {
+    yield takeLatest(LOAD_POST_REQUEST, loadPost);
+}
 export default function* postSaga() {
     yield all([
         fork(watchAddPost),
         fork(watchRemovePost),
         fork(watchAddComment),
+        fork(watchLoadPost),
     ]);
 }
