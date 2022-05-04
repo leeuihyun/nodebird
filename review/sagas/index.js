a874e66f9a21012c9499b6c103ffebd2d650dac1
leeuihyun <meta212121@naver.com>
2022-05-05 00:28:30 +0900

    edit index saga edit axios.defaults.baseURL

--- review/sagas/index.js
+++ review/sagas/index.js
@@ -1,6 +1,9 @@
 import userSaga from "./user";
 import postSaga from "./post";
 import { all, fork } from "redux-saga/effects";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3065";
 
 export default function* rootSaga() {
     yield all([fork(userSaga), fork(postSaga)]);
