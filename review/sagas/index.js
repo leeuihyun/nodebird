a322d9eaaa4e7fa150b6722feac8ebe2887d5abe
leeuihyun <meta212121@naver.com>
2022-05-08 18:41:23 +0900

    ⚡edit index.js + add axios withCredentials true

--- review/sagas/index.js
+++ review/sagas/index.js
@@ -4,6 +4,7 @@ import { all, fork } from "redux-saga/effects";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3065";
+axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
     yield all([fork(userSaga), fork(postSaga)]);
