57ebe00310528c97188a1ee6bac6c820091e90cd
leeuihyun <meta212121@naver.com>
2022-05-25 15:40:05 +0900

    back aws change

--- prepare2/back/app.js
+++ prepare2/back/app.js
@@ -12,6 +12,8 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const postsRouter = require("./routes/posts");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 dotenv.config();
 
@@ -25,6 +27,13 @@ db.sequelize
     .catch(console.error);
 
 passportConfig();
+if (process.env.NODE_ENV === "production") {
+    app.use(morgan("combined"));
+    app.use(hpp());
+    app.use(helmet());
+} else {
+    app.use(morgan("dev"));
+}
 app.use(morgan("dev"));
 app.use(
     cors({
