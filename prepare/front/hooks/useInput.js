5914a4948626fe82db91ae9f10466e2ae7222949
leeuihyun <meta212121@naver.com>
2022-05-25 17:30:33 +0900

    change

--- prepare/front/hooks/useInput.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from "react";
-
-export default (initialValue = null) => {
-    const [value, setter] = useState(initialValue);
-    const handler = useCallback((e) => {
-        setter(e.target.value);
-    }, []);
-    return [value, handler, setter];
-};
